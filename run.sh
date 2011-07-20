9efacc86855751d90f4384f1762bedd779e22a00
Reldan(Eldar Nugaev) <eldr@ya.ru>
2011-07-20 18:30:33 +0400

    fix

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 # formating EBS
 grep -q ext3 /proc/filesystems || sudo modprobe ext3
 sudo mkfs.ext3 /dev/sdf
-echo "dev/sdf /vol ext3 noatime 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdf /vol ext3 noatime 0 0" | sudo tee -a /etc/fstab
 sudo mkdir -m 0700 /vol
 sudo mount /vol

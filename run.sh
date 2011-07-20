105dcda380f3dee8439629aa24441c3554aa31c4
Reldan(Eldar Nugaev) <eldr@ya.ru>
2011-07-20 18:27:01 +0400

    First commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+# formating EBS
+grep -q ext3 /proc/filesystems || sudo modprobe ext3
+sudo mkfs.ext3 /dev/sdf
+echo "dev/sdf /vol ext3 noatime 0 0" | sudo tee -a /etc/fstab
+sudo mkdir -m 0700 /vol
+sudo mount /vol
